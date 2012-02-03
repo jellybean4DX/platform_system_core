833496aaa1443fd0790ee6a460c60ca3033524bd
dhacker29 <davidhackerdvm@gmail.com>
2012-02-03 15:44:11 -0600

    Missing moto ids

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -76,7 +76,7 @@
 #define AID_NET_BW_STATS  3006  /* read bandwidth statistics */
 #define AID_NET_BW_ACCT   3007  /* change bandwidth statistics accounting */
 
-//#ifdef USE_MOTOROLA_USERS
+#ifdef USE_MOTOROLA_USERS
 #define AID_MOT_OSH       5000  /* OSH */
 #define AID_MOT_ACCY      9000  /* access to accessory */
 #define AID_MOT_PWRIC     9001  /* power IC */
@@ -156,6 +156,22 @@ static const struct android_id_info android_ids[] = {
     { "mot_caif",     AID_MOT_CAIF, },
     { "mot_dlna",     AID_MOT_DLNA,},
 //#endif
+    { "qcom_oncrpc", AID_QCOM_ONCRPC, },
+#if defined(MOTOROLA_UIDS)
+    { "mot_osh",   AID_MOT_OSH, },
+    { "mot_accy",  AID_MOT_ACCY, },
+    { "mot_pwric", AID_MOT_PWRIC, },
+    { "mot_usb",   AID_MOT_USB, },
+    { "mot_drm",   AID_MOT_DRM, },
+    { "mot_tcmd",  AID_MOT_TCMD, },
+    { "mot_sec_rtc",   AID_MOT_SEC_RTC, },
+    { "mot_tombstone", AID_MOT_TOMBSTONE, },
+    { "mot_tpapi",     AID_MOT_TPAPI, },
+    { "mot_secclkd",   AID_MOT_SECCLKD, },
+    { "mot_whisper",   AID_MOT_WHISPER, },
+    { "mot_caif",  AID_MOT_CAIF, },
+    { "mot_dlna",  AID_MOT_DLNA, },
+#endif
     { "misc",      AID_MISC, },
     { "nobody",    AID_NOBODY, },
 };
